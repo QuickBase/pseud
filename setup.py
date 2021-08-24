60d63757df8a748be4be69e5c4a3218939c12bf8
Ivan Ivanov <iivanov@quickbase.com>
2021-08-24 15:47:59 +0300

    Bump up version
    The version number is now compatible with PEP-440. We use local
    numbers for our changes.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_that_file(path):
 long_description = '\n'.join((read_that_file('README.rst'),
                               read_that_file('LICENSE.txt')))
 
-version = '1.0.0-nt10'
+version = '1.0.0+qb.11'
 
 
 setup(name='pseud',
